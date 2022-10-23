79b7a74b4e417b8db88bba5ecff0e807dbb1c687
nanikit <nanikit4@gmail.com>
2022-10-23 17:37:48 +0900

    fix: null reference exception

--- functions/api/profile/[id].ts
+++ functions/api/profile/[id].ts
@@ -1,12 +1,12 @@
 export const onRequestGet = async (context: EventContext<{}, 'id', {}>) => {
   const { request, params } = context;
-  const origin = request.headers.get('Origin');
+  const origin = request.headers.get('Origin') as string | null;
   try {
     const response = await fetch(`https://api.beatleader.xyz/player/${params.id}`);
 
     return new Response(response.body, {
       headers: {
-        ...(origin.startsWith('http://localhost')
+        ...(origin?.startsWith('http://localhost')
           ? {
               'Access-Control-Allow-Origin': origin,
               'Access-Control-Allow-Methods': 'GET',
@@ -18,11 +18,15 @@ export const onRequestGet = async (context: EventContext<{}, 'id', {}>) => {
   } catch (err) {
     return new Response(`${err.message}\n${err.stack}`, {
       status: 500,
-      headers: {
-        'Access-Control-Allow-Origin': origin,
-        'Access-Control-Allow-Methods': 'GET',
-        Vary: 'Origin',
-      },
+      ...(origin
+        ? {
+            headers: {
+              'Access-Control-Allow-Origin': origin,
+              'Access-Control-Allow-Methods': 'GET',
+              Vary: 'Origin',
+            },
+          }
+        : {}),
     });
   }
 };
