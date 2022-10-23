012e830062e75d80d438a1212af4ebde412919da
nanikit <nanikit4@gmail.com>
2022-10-23 17:29:50 +0900

    feat: add error handling

--- functions/api/profile/[id].ts
+++ functions/api/profile/[id].ts
@@ -1,17 +1,28 @@
 export const onRequestGet = async (context: EventContext<{}, 'id', {}>) => {
   const { request, params } = context;
   const origin = request.headers.get('Origin');
-  const response = await fetch(`https://api.beatleader.xyz/player/${params.id}`);
+  try {
+    const response = await fetch(`https://api.beatleader.xyz/player/${params.id}`);
 
-  return new Response(response.body, {
-    headers: {
-      ...(origin.startsWith('http://localhost')
-        ? {
-            'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Methods': 'GET',
-            Vary: 'Origin',
-          }
-        : {}),
-    },
-  });
+    return new Response(response.body, {
+      headers: {
+        ...(origin.startsWith('http://localhost')
+          ? {
+              'Access-Control-Allow-Origin': origin,
+              'Access-Control-Allow-Methods': 'GET',
+              Vary: 'Origin',
+            }
+          : {}),
+      },
+    });
+  } catch (err) {
+    return new Response(`${err.message}\n${err.stack}`, {
+      status: 500,
+      headers: {
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Methods': 'GET',
+        Vary: 'Origin',
+      },
+    });
+  }
 };
